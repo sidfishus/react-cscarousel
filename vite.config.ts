d79257522d339c02dd3a7bb6ed1d4caba6edc99b
Sid <sidnet@gmail.com>
2024-12-22 13:52:18 +0000

    Testing module import

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc"
+//import typescript from "rollup-plugin-typescript2"
+import dts from "vite-plugin-dts"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),dts({ tsconfigPath: './tsconfig.app.json' })],
   build: {
     lib: {
       name: "react-cscarousel",
       entry: ["src/carousel/index.tsx"],
-      formats: ["es", "umd"]
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ["react", "react-dom","react/jsx-runtime"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           react: "react",
           "react-dom": "react-dom",
+          "react/jsx-runtime": "jsxRuntime",
         },
       },
     },
+    sourcemap: true,
   },
 })
